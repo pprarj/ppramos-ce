6b24782d08e2cff606df06fa73baa03688133819
Pedro Paulo Ramos Junior <pedro.ramos@iv2.com.br>
2017-12-13 16:28:45 -0200

    Ajustes no menu e bloco de notas auxiliar

--- views/menu.php
+++ views/menu.php
@@ -1,5 +1,5 @@
 <ul>
     <a href="<?php echo BASE_URL; ?>/"><li>Página Inicial</li></a>
-    <a href="<?php echo BASE_URL; ?>/"><li>Cadastrar produtos</li></a>
-    <a href="<?php echo BASE_URL; ?>/"><li>Listar produtos</li></a>
+    <a href="<?php echo BASE_URL; ?>/produtos/cadastrar"><li>Cadastrar produtos</li></a>
+    <a href="<?php echo BASE_URL; ?>/produtos/listar"><li>Listar produtos em estoque</li></a>
 </ul>
